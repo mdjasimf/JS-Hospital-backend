c6b68794f5584f3da0b883ca71e3ccef9dec09b9
mdJasimf <mdjasimf@gmail.com>
2024-09-07 10:34:01 +0600

    off

--- src/app/modules/specialties/specialties.route.ts
+++ src/app/modules/specialties/specialties.route.ts
@@ -21,7 +21,7 @@ router.post(
 );
 router.delete(
   '/:id',
-  // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   SpecialtiesController.deleteFromDB,
 );
 
